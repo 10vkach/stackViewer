93bc67ac6c07982b5ed42de3d3c8eb4cd17eba6c
Lovkach <Lovkach@outlook.com>
2019-11-03 15:00:34 +0400

    Создание проекта

---
+++ stackViwer/stackViwer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackViwer
+//
+//  Created by mobileBuilder on 03.11.2019.
+//  Copyright © 2019 mobileBuilder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
